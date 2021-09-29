02c8bb0978921a188f1eb75183c87f022398a6f3
PonikTrofik <12055230+Tonikprofik@users.noreply.github.com>
2021-09-29 14:03:57 +0200

    Formsubmission collection
    Formsubmission added also to pld.config

---
+++ collections/FormSubmission.ts
@@ -0,0 +1,38 @@
+import {CollectionConfig} from 'payload/types';
+
+const FormSubmission: CollectionConfig = {
+
+    slug: 'form-submissions',
+    fields: [
+      {
+        type: 'text',
+        name: 'email',
+        label: 'From Email',
+        admin: {
+          readOnly: true,
+
+        },
+
+      },
+      {
+        type:'textarea',
+        name: 'message',
+        label: 'Message',
+        admin: {
+          readOnly: true,
+        }
+      },
+      {
+        type: 'text',
+        name: 'source',
+        label: 'Source',
+
+        admin: {
+          position: 'sidebar',
+          readOnly: true
+        }
+      }
+    ]
+ };
+
+export default FormSubmission
