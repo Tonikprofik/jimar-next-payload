ebd95bda5538859b3562bc837dd7e359b43e7827
PonikTrofik <izanami369@gmail.com>
2021-10-10 19:26:48 +0200

    extended document init from mui example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import type { AppProps } from 'next/app';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import createEmotionCache from '../styles/createEmotionCache';
+// import create theme from '../styles/theme';
 
-import '../css/style.css';
+// import '../css/style.css';
+
+const clientSideEmotionCache = createEmotionCache();
 
 const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => (
+
+
+
   <Component {...pageProps} />
 );
 
