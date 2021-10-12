f401ac084ee027c3460fab5cf4a1917858bed5a8
PonikTrofik <izanami369@gmail.com>
2021-10-12 15:36:13 +0200

    feat: ProductList layout, checkout

--- components/layout/ProductList/index.js
+++ components/layout/ProductList/index.js
@@ -14,16 +14,43 @@ import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 
 export default function ProductList() {
-
-    const cards = [1,2,3,4,5,6 ]
+  const cards = [1, 2, 3, 4, 5, 6];
   return (
     <>
-      <Box sx={{ width: 200, height: 200 }}>
+      <Box>
         <Container>
-          <Grid container spacing={4} sx={{ display: "inline-block" }}>
-            <Grid item> 1</Grid>
-            <Grid item> 2</Grid>
-            <Grid item> 3</Grid>
+          <Grid container spacing={4} sx={{ display: "block" }}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card
+                  sx={{
+                    height: "100%",
+                    display: "flex",
+                    flexDirection: "column",
+                  }}
+                >
+                  <CardMedia
+                    components="img"
+                    sx={{ pt: "56.25%" }}
+                    image="https://source.unsplash.com/random"
+                    alt="random"
+                  />
+                  <CardContent sx={{ flexGrow: 1 }}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe
+                      the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small">View</Button>
+                    <Button size="small">Edit</Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
           </Grid>
         </Container>{" "}
         product list here
